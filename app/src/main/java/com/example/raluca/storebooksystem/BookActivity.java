e249a782c5fd5aac6fd05471ea51a817da41be6d
Raluca <Raluca_mih97@yahoo.com>
2017-12-28 00:54:00 +0200

    Inceputul interfetei BookActivity pana la butonul portocaliu

--- app/src/main/java/com/example/raluca/storebooksystem/BookActivity.java
+++ app/src/main/java/com/example/raluca/storebooksystem/BookActivity.java
@@ -3,39 +3,41 @@ package com.example.raluca.storebooksystem;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import model.Book;
 
 public class BookActivity extends AppCompatActivity {
     private Book book;
     private ImageView imageView;
-    private EditText m_editTextBook;
+    private TextView m_textViewTitle;
+    private TextView m_textViewAuthor;
     private int imageNumber;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_book_activity);
-        imageView=(ImageView)findViewById(R.id.imageView);
-        m_editTextBook=(EditText)findViewById(R.id.editTextBook);
+        imageView = (ImageView) findViewById(R.id.imageView);
+        m_textViewTitle = (TextView) findViewById(R.id.textViewTitle);
+        m_textViewAuthor = (TextView) findViewById(R.id.textViewAuthor);
 
         Intent intent = getIntent();
         book = (Book) intent.getSerializableExtra("book");
 
         Bundle bundle = intent.getExtras();
 
-        if(bundle!=null)
-        {
-            imageNumber =(int) bundle.get("imageNumber");
+        if (bundle != null) {
+            imageNumber = (int) bundle.get("imageNumber");
         }
 
-       // imageNumber=(int)intent.getSerializableExtra("imageNumber");
+        // imageNumber=(int)intent.getSerializableExtra("imageNumber");
 
-        m_editTextBook.setText(book.getTitle());
+         m_textViewTitle.setText(book.getTitle());
+        m_textViewAuthor.setText(book.getAuthor());
 
-        int resID = getResources().getIdentifier(book.getNamePicture(), "drawable",  getPackageName());
+        int resID = getResources().getIdentifier(book.getNamePicture(), "drawable", getPackageName());
         imageView.setImageResource(resID);
 
     }
